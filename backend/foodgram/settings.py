f0f2948c20569069d453b5b92968565084a0f51e
Наиля Морданшина <alnailya27gmail.com>
2023-12-25 01:11:41 +0600

    [docker]

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -173,7 +173,7 @@ STATIC_ROOT = BASE_DIR / 'backend_static/static'
 STATIC_URL = '/static/'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = BASE_DIR / '../media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
